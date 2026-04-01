1877e5722bea6304820632cf2f66371be494899e
daniyal379 <daniimugh@gmail.com>
2026-04-01 17:28:48 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+// let clas = {
+//     name: "Daniyal",
+//     rollno: 345,
+// }
+// console.log(clas.rollno)
+// // prompt("Enter your name");
+// let name = prompt("Enter Your Name");
+// if(name === "Daniyal")
+// {
+//     alert("Hello Daniyal");
+// }
+// else{
+//     alert("You are not allowed")
+// }
